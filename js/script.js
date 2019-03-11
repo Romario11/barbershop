3e20f163bbf01dea988141bfaf5df6ce5c94c6b0
Romario <rsteamdev@gmail.com>
2019-03-11 22:53:59 +0100

    update script file

--- js/script.js
+++ js/script.js
@@ -8,38 +8,6 @@ if (true) {
 		else{}
 }
 
-class People{
-	var name;
-	var age;
-}
-
-export class Message {
-  constructor(public author: string, public content: string) {}
-}
-@Injectable()
-export class ChatService {
-  constructor() {}
-  conversation = new Subject<Message[]>();
-  messageMap = {
-    "Hi": "Hello",
-    "Who are you": "My name is Agular Bot",
-    "What is Angular": "Angular is the best framework ever",
-    "default": "I can't understand. Can you please repeat"
-  }
-  getBotAnswer(msg: string) {
-    const userMessage = new Message('user', msg);
-    this.conversation.next([userMessage]);
-    const botMessage = new Message('bot', this.getBotMessage(msg));
-    setTimeout(()=>{
-      this.conversation.next([botMessage]);
-    }, 1500);
-  }
-  getBotMessage(question: string){
-    let answer = this.messageMap[question];
-    return answer || this.messageMap['default'];
-  }
-}
-
 function removeChar(str){
 var arr = str.split('');
  arr.shift();
